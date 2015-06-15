a6053d8a540a90375125f17e862982a423cb91f3
thinkholic <mailtokasun@gmail.com>
2015-06-15 17:14:04 +0530

    Initial Commit

---
+++ packages/thinkholic:alerts/package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: "thinkholic:alerts",
+  version: "0.0.1",
+  summary: "Bootstrap Alerts. Before use this package, you need to add meteor add twbs:bootstrap package first.",
+  git: "https://github.com/thinkholic/meteor-bootstrap-alerts",
+  documentation: ""
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom("1.1.0.2");
+  api.use("templating", "client");
+  api.addFiles("view.html", "client");
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('thinkholic:alerts');
+  api.addFiles('alerts-tests.js');
+});
